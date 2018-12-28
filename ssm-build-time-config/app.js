3d39168e981698b111980dbb53658f6daf90d064
marcinzasepa <marcin.zasepa@homegate.ch>
2018-12-28 17:37:33 +0100

    Add Secret Manager Parameter reference

--- ssm-build-time-config/app.js
+++ ssm-build-time-config/app.js
@@ -5,7 +5,8 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: `Here is the value from env variable: ${process.env.MY_PARAM_FROM_SSM}`
+                message: `Here is the value from env variable delivered from ssm: ${process.env.MY_PARAM_FROM_SSM} 
+                and from secret manager: ${process.env.MY_PARAM_FROM_SECRET_MANAGER}`
             })
         }
     } catch (err) {
