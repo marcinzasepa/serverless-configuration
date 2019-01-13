00690d400077e0adaef3d007feedf9c2a90577c7
marcinzasepa <marcin.zasepa@homegate.ch>
2019-01-13 22:53:41 +0100

    Add read from Secret Manager via Paramter Store API

--- ssm-run-time-config/app.js
+++ ssm-run-time-config/app.js
@@ -3,21 +3,24 @@ const AWS = AWSXRay.captureAWS(require('aws-sdk'));
 
 const ssm = new AWS.SSM();
 
-const request = {
-    Path: `/serviceA`,
-    Recursive: true,
-    WithDecryption: false,
-};
-
 let response;
 exports.lambdaHandler = async (event, context) => {
     try {
 
-        const result = await ssm.getParametersByPath(request).promise();
+        const result = await ssm.getParametersByPath({
+            Path: `/serviceA`,
+            Recursive: true,
+            WithDecryption: false,
+        }).promise();
+        const parameterFromSecretManager = await ssm.getParameter({
+            Name: '/aws/reference/secretsmanager/secretValue',
+            WithDecryption: true
+        }).promise();
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: `Here are the values from ssm: ${JSON.stringify(result.Parameters)}`
+                message: `Here are the values from ssm: ${JSON.stringify(result.Parameters)} 
+                and parameter value from secret manager via ssm: ${JSON.stringify(parameterFromSecretManager)}`
             })
         };
     } catch (err) {
